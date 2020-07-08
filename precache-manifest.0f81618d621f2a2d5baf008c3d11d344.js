72f1714f1f4a991ade69fd6317bcfea6d48ee5a6
juanmanuelcrego <juanmanuel.crego@gmail.com>
2020-07-08 01:56:14 -0300

    Updates

---
+++ precache-manifest.0f81618d621f2a2d5baf008c3d11d344.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35013e6b794640282d376adfa5a30e3d",
+    "url": "/power/index.html"
+  },
+  {
+    "revision": "932779a1394cde67c484",
+    "url": "/power/static/css/main.959cc323.chunk.css"
+  },
+  {
+    "revision": "6c6e97a8497500693b7c",
+    "url": "/power/static/js/2.4de2f9e6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/power/static/js/2.4de2f9e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932779a1394cde67c484",
+    "url": "/power/static/js/main.75b264de.chunk.js"
+  },
+  {
+    "revision": "fadae89b4802d15adf66",
+    "url": "/power/static/js/runtime-main.b8851914.js"
+  },
+  {
+    "revision": "843ef85f7b9d895dc843177b26a1154c",
+    "url": "/power/static/media/Logo.843ef85f.png"
+  },
+  {
+    "revision": "9b4f706158c2f6eb1af348e8d367cd4a",
+    "url": "/power/static/media/video.9b4f7061.mp4"
+  }
+]);
